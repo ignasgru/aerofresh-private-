c770c5c57e19e89fabd0bb730c936ad976b3ec72
ignasgru <38707230+ignasgru@users.noreply.github.com>
2025-09-16 22:29:01 -0700

    feat(web): add basic index page

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Aerofresh Dashboard</h1>
+      <p>Welcome to Aerofresh web dashboard. This app lets you search for aircraft, view details and more.</p>
+    </div>
+  );
+}
